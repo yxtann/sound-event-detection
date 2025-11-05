e76b80e5f8c2e6b9fb5b8043b0f77bb0dd43ed38
Nicole Tann <tannlilin@gmail.com>
2025-11-06 01:17:53 +0800

    added processing to spectrogram

---
+++ src/utils/audio_to_spectrograms.py
@@ -0,0 +1,30 @@
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+
+def get_mel_spec(filename, path, sr, n_fft = 2048, hop_length = 512, n_mels = 64):
+    y, sr = librosa.load(path +'/'+ filename, sr = sr)
+    fmax = sr // 2
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft,hop_length=hop_length, n_mels=n_mels, fmin=0, fmax=fmax, power=2.0)
+    S_db = librosa.power_to_db(S, ref=np.max)  # log scale (dB)
+    return S_db
+
+def get_stft_spec(filename, path, sr):
+    y, sr = librosa.load(path +'/'+ filename, sr = sr)
+    D = np.abs(librosa.stft(y))
+    S_db = librosa.amplitude_to_db(D, ref=np.max)
+    return S_db
+
+def display_spectrogram(S_db, sr, annot_filter = None, filename = None):
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(S_db, sr=sr, x_axis='time', y_axis='hz')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectrogram')
+    if annot_filter is not None:
+        if filename is None:
+            raise Exception('Please specify file name')
+        annot_file = annot_filter.query(f'filename == "{filename}"')
+        plt.axvline(annot_file['onset'].iloc[0])
+        plt.axvline(annot_file['offset'].iloc[0])
+    plt.tight_layout()
+    plt.show()
