1d868e65b10c7b83e2f48e575cf3c041dd4abb8f
Nicole Tann <tannlilin@gmail.com>
2025-12-06 18:26:18 +0800

    integrate yamnet into main

--- src/models/yamnet_train.py
+++ src/models/yamnet_train.py
@@ -14,6 +14,10 @@ from loguru import logger
 
 from tqdm import tqdm
 from pathlib import Path
+
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..\..')))
 from src.config import DETECTION_TRAIN_PATH, DETECTION_TEST_PATH
 from src.utils.audio_to_spectrograms import create_spectrogram_pkl
 
